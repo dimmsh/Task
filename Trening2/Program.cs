e5988a99bb077c22d9f1ed6e671e80a31a4cdf12
dimmsh <dshust5@gmail.com>
2019-08-27 20:50:51 +0300

    add Task1Trening2

---
+++ Trening2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trening2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Draw();
+
+            Square square = new Square();
+            square.Draw();
+
+            Console.ReadKey();
+        }
+
+        public abstract class Figure
+        {
+            public abstract void Draw();
+        }
+        public class Square : Figure
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("Це метод з Square классу");
+            }
+        }
+        public class Rectangle : Figure
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("Це метод з Rectanle классу");
+            }
+        }
+    }
+}
+
+
