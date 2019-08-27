e4b38cbf0d65a2476a1c13ede003859505131d89
dimmsh <dshust5@gmail.com>
2019-08-27 20:37:43 +0300

    addTask2Trening2

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class Cirkle
+    { 
+       int radiys;
+
+       const double P = 3.14159;
+
+       public MethodDovkola (radiys)
+       {
+           
+       }
+    }
+}
---
+++ Task2Trening2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2Trening2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(1, 5, 7);
+            rectangle.Display();
+
+            rectangle.Draw();
+
+            Square square = new Square(5, 10, 6 );
+            square.Display();
+            square.Draw();
+
+            Console.ReadKey();
+        }
+
+        public abstract class Figure
+        {
+            public int X { get; set;  }
+
+            public int Y { get; set; }
+            
+            public Figure(int x, int y)
+            {
+                X = x;
+                Y = y;
+            } 
+
+            public abstract void Draw();
+            public void Display()
+            {
+                Console.WriteLine(X);
+                Console.WriteLine(Y);
+            }
+        }
+        public class Square : Figure
+        {
+            public int Sum { get; set; }
+
+            public Square(int x, int y, int sum) : base ( x, y) 
+            {
+                Sum = sum;
+            }
+            public override void Draw()
+            {
+                Console.WriteLine("Це метод з Square классу");
+            }
+        }
+        public class Rectangle : Figure
+        {
+            public int Position { get; set; }
+
+            public Rectangle(int x, int y, int position) : base ( x, y) 
+            {
+                Position = position;
+            }
+            public override void Draw()
+            {
+                Console.WriteLine("Це метод з Rectanle классу");
+            }
+        }
+    }
+}
